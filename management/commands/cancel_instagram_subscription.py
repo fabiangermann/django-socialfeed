54b39638bc1edb9798a6566af5bb4b21a3857d70
Fabian Germann <fg@feinheit.ch>
2014-07-22 14:20:06 +0200

    Add management commands for listing and deleting subscriptions

---
+++ management/commands/cancel_instagram_subscription.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+
+from instagram.client import InstagramAPI
+
+from ...models import Subscription
+
+class Command(BaseCommand):
+    def handle(self, app_id=None, app_secret=None, subscription_id=None, *args, **kwargs):
+        if app_id and app_secret and subscription_id:
+
+            api = InstagramAPI(
+                client_id=app_id,
+                client_secret=app_secret)
+            print '\nDeleting subscription {}'.format(subscription_id)
+            api.delete_subscriptions(id=subscription_id)
---
+++ management/commands/list_instagram_subscriptions.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+
+from instagram.client import InstagramAPI
+
+from ...models import Subscription
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        subscriptions = Subscription.objects.filter(provider__in=[
+            'socialfeed.providers.instagram.hashtag'
+        ])
+
+        processed_apps = []
+        for subscription in subscriptions:
+            app_id = subscription.config.get('app_id')
+            app_secret = subscription.config.get('app_secret')
+
+
+            if app_id and app_secret and app_id not in processed_apps:
+                api = InstagramAPI(
+                    client_id=app_id,
+                    client_secret=app_secret)
+                print '\nSubscriptions for app {}'.format(app_id)
+                print api.list_subscriptions()
+
+                processed_apps.append(app_id)

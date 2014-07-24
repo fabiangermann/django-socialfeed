5820f531a6d0cb1877f6f713af7036dfbc33d0fe
Fabian Germann <fg@feinheit.ch>
2014-07-24 14:45:58 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, find_packages
+
+setup(
+    name='django-socialfeed',
+    version='0.1',
+    description='',
+    author='Fabian Germann',
+    author_email='fg@feinheit.ch',
+    url='feinheit.ch',
+    packages=find_packages()
+)

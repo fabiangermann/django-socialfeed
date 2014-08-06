904107eb18e1a78dacbde154ffef68d2fa22bcad
Fabian Germann <fg@feinheit.ch>
2014-08-06 16:42:19 +0200

    Add comment to subscribe/unsubscribe stubs, add warning not to save subscription in there

--- socialfeed/providers/__init__.py
+++ socialfeed/providers/__init__.py
@@ -12,9 +12,20 @@ class BaseProvider(object):
         super(BaseProvider, self).__init__(*args, **kwargs)
 
     def subscribe(self):
+        '''
+        Called when the is_active flag is set from false to true (on save).
+
+        DO NOT save the subscription in this method!
+        '''
         raise NotImplementedError
 
     def unsubscribe(self):
+        '''
+        Called when the is_active flag is set from true to false (on save)
+        and when subscription is delted.
+
+        DO NOT save the subscription in this method!
+        '''
         raise NotImplementedError
 
     def pull_posts(self):
